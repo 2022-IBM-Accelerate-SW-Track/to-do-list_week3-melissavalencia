0e830e9ddeab81b9d374d65e88a6510258aa7f16
Melissa Valencia <melissa.valencia.23@dartmouth.edu>
2022-06-24 19:37:38 -0700

    lab 3 done

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -16,28 +16,86 @@ afterEach(() => {
   container = null;
 });
 
-
-
-
- test('test that App component doesn\'t render dupicate Task', () => {
+test('that App component doesn\'t render dupicate Task', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "12/30/2023";
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getAllByText(/Test/i);
+  expect(check.length).toBe(1);
+});
 
- test('test that App component doesn\'t add a task without task name', () => {
+test('that App component doesn\'t add a task without task name', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "12/30/2023";
+  fireEvent.change(inputTask, { target: { value: ""}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+});
 
- test('test that App component doesn\'t add a task without due date', () => {
+test('that App component doesn\'t add a task without due date', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: ""}});
+  fireEvent.click(element);
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+});
 
-
-
- test('test that App component can be deleted thru checkbox', () => {
+test('that App component can be deleted thru checkbox', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "12/30/2023";
+  fireEvent.change(inputTask, { target: { value: "Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const checkTask = screen.getByText(/Test/i);
+  expect(checkTask).toBeInTheDocument();
+  const checkBox = screen.getByRole('checkbox');
+  fireEvent.click(checkBox);
+  const checkDelete = screen.getByText(/You have no todo's left/i);
+  expect(checkDelete).toBeInTheDocument();
+});
 
-
- test('test that App component renders different colors for past due events', () => {
+test('that App component renders different colors for past due events', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const pastDue = "12/30/2021";
+  fireEvent.change(inputTask, { target: { value: "Pink"}});
+  fireEvent.change(inputDate, { target: { value: pastDue}});
+  fireEvent.click(element);
+  const currDue = "12/30/2023";
+  fireEvent.change(inputTask, { target: { value: "White"}});
+  fireEvent.change(inputDate, { target: { value: currDue}});
+  fireEvent.click(element);
+  const checkPast = screen.getByText(/Pink/i);
+  const checkPastDate = screen.getByText(new RegExp(pastDue,"i"));
+  expect(checkPast).toBeInTheDocument();
+  expect(checkPastDate).toBeInTheDocument();
+  const checkCurr = screen.getByText(/White/i);
+  const checkCurrDate = screen.getByText(new RegExp(currDue,"i"));
+  expect(checkCurr).toBeInTheDocument();
+  expect(checkCurrDate).toBeInTheDocument();
+  const pastColor = screen.getByTestId(/Pink/i).style.background;
+  const currColor = screen.getByTestId(/White/i).style.background;
+  expect(pastColor).not.toBe(currColor);
+});
